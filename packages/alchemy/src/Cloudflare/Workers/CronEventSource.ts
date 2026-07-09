489cbcd1d09d04a489d429a425937905a021b88f
Sam Goodwin <sam@alchemy.run>
2026-07-09 12:22:40 -0700

    remove helper type and place docs in right place

--- packages/alchemy/src/Cloudflare/Workers/CronEventSource.ts
+++ packages/alchemy/src/Cloudflare/Workers/CronEventSource.ts
@@ -198,25 +198,20 @@ import { isWorkerEvent, Worker } from "./Worker.ts";
  *
  * @see https://developers.cloudflare.com/workers/configuration/cron-triggers/
  */
-type CronEventSourceRequirements<Req> = Exclude<Req, RuntimeContext>;
-
 export const cron = <Req = never>(
   expression: string,
   process: (
     controller: cf.ScheduledController,
   ) => Effect.Effect<void, unknown, Req>,
-): Effect.Effect<
-  void,
-  never,
-  CronEventSource | CronEventSourceRequirements<Req>
-> => CronEventSource.use((source) => source(expression, process));
+): Effect.Effect<void, never, CronEventSource | Exclude<Req, RuntimeContext>> =>
+  CronEventSource.use((source) => source(expression, process));
 
 export type CronEventSourceService = <Req = never>(
   expression: string,
   process: (
     controller: cf.ScheduledController,
   ) => Effect.Effect<void, unknown, Req>,
-) => Effect.Effect<void, never, CronEventSourceRequirements<Req>>;
+) => Effect.Effect<void, never, Exclude<Req, RuntimeContext>>;
 
 export class CronEventSource extends Context.Service<
   CronEventSource,
